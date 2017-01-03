dabe1e0251a30eb86d794d6deb8cd217a581ca35
dereuromark <euromark@web.de>
2017-01-03 02:13:12 +0100

    Add behavior test.

---
+++ tests/Fixture/EventsFixture.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Calendar\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * EventsFixture
+ */
+class EventsFixture extends TestFixture {
+
+	/**
+	 * Fields
+	 *
+	 * @var array
+	 */
+	// @codingStandardsIgnoreStart
+	public $fields = [
+		'id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+		'title' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'description' => ['type' => 'text', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+		'beginning' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+		'end' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+		'_constraints' => [
+			'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+		],
+	];
+
+	/**
+	 * Records
+	 *
+	 * @var array
+	 */
+	public $records = [
+		[
+			'id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet',
+			'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'beginning' => '2015-08-17 15:32:36',
+			'end' => '2015-08-17 15:32:36',
+		],
+	];
+
+}
---
+++ tests/TestCase/Model/Behavior/CalendarBehaviorTest.php
@@ -0,0 +1,131 @@
+<?php
+namespace Calendar\Test\Model\Behavior;
+
+use Cake\Controller\Controller;
+use Cake\Datasource\ConnectionManager;
+use Cake\Network\Exception\InternalErrorException;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+class CalendarBehaviorTest extends TestCase {
+
+	/**
+	 * @var array
+	 */
+	public $fixtures = [
+		'plugin.Calendar.Events'
+	];
+
+	/**
+	 * @var \Cake\ORM\Table;
+	 */
+	public $Events;
+
+	/**
+	 * @var array
+	 */
+	public $config = [
+		'field' => 'beginning'
+	];
+
+	/**
+	 * setUp
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->Events = TableRegistry::get('Calendar.Events');
+		$this->Events->addBehavior('Calendar.Calendar', $this->config);
+
+		$this->db = ConnectionManager::get('test');
+
+		$this->_addFixtureData();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function tearDown() {
+		parent::tearDown();
+
+		unset($this->Events);
+		TableRegistry::clear();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testFind() {
+		$options = [
+			'month' => 12,
+			'year' => (int)date('Y'),
+		];
+
+		$events = $this->Events->find('calendar', $options);
+
+		$eventList = array_values($events->find('list')->toArray());
+		$expected = [
+			'One',
+			'4 days',
+			'Over new years eve',
+		];
+		$this->assertEquals($expected, $eventList);
+	}
+
+	/**
+	 * Gets a new Entity
+	 *
+	 * @param array $data
+	 * @return \Cake\ORM\Entity
+	 */
+	protected function _getEntity($data) {
+		return new Entity($data);
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function _addFixtureData()
+	{
+		$data = [
+			[
+				'title' => 'Wrong',
+				'beginning' => date('Y') . '-11-30',
+			],
+			[
+				'title' => 'One',
+				'beginning' => date('Y') . '-12-28',
+			],
+			[
+				'title' => '4 days',
+				'beginning' => date('Y') . '-12-14',
+				'end' => date('Y') . '-12-18',
+			],
+			[
+				'title' => 'Over new years eve',
+				'beginning' => date('Y') . '-12-29',
+				'end' => (date('Y') + 1) . '-01-02',
+			],
+		];
+
+		foreach ($data as $row) {
+			$entity = $this->_getEntity($row);
+			if (!$this->Events->save($entity)) {
+				throw new InternalErrorException(print_r($entity->errors()));
+			}
+		}
+	}
+
+}
+
+class TestController extends Controller {
+
+	/**
+	 * @var string
+	 */
+	public $modelClass = 'Calendar.Events';
+
+}
