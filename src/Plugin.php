72006ab0a226857711f4f2ad3813340bd981a316
mscherer <mark.scherer@spryker.com>
2020-03-18 00:15:48 +0100

    Disable hooks.

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,4 +5,25 @@ namespace Calendar;
 use Cake\Core\BasePlugin;
 
 class Plugin extends BasePlugin {
+
+	/**
+	 * @var bool
+	 */
+	protected $middlewareEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $consoleEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $bootstrapEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $routesEnabled = false;
+
 }
